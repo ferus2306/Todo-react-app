847a341f91feb165776627e4fc31ad958bb681f6
Firus Hanov <d00363593@dmail.dixie.edu>
2020-05-20 16:58:46 -0600

    adding and removing items functionalities are added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 class App extends Component {
   state = {
@@ -9,14 +10,30 @@ class App extends Component {
     ]
   }
 
+  deleteTodo = (id) => {
+    // console.log('Deleted')
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    }) 
+  }
 
+  addTodo = (todo) => {
+    // console.log('adding an item')
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos: todos
+    })
+  }
     render() {
       return (
       <div className="todo-app container">
-
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos}/>
-        
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo = {this.addTodo}/>
       </div>
     );
   }
